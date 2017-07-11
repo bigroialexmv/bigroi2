3b2aa5cd1fcfc927f8684238ecd783cae764609a
Alexander <Alexander@Alexander-PC>
2017-07-11 19:52:05 +0300

    database project

---
+++ bigroi_shop_database/Scripts/data_load.sql
@@ -0,0 +1,103 @@
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Xiaomi Redmi Note 4X 32GB Black', 395, 10, CURRENT_TIMESTAMP, 
+'Android, экран 5.5" IPS (1080x1920), ОЗУ 3 ГБ, флэш-память 32 ГБ, карты памяти, камера 13 Мп, аккумулятор 4100 мАч, 2 SIM, цвет черный');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Xiaomi Redmi 4X 16GB Black', 249, 10, CURRENT_TIMESTAMP, 'Android, экран 5" IPS (720x1280), ОЗУ 2 ГБ, флэш-память 16 ГБ, карты памяти, камера 13 Мп, аккумулятор 4100 мАч, 2 SIM, цвет черный');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Apple iPhone 7 32GB Black', 1399, 10, CURRENT_TIMESTAMP, 'Apple iOS, экран 4.7" IPS (750x1334), ОЗУ 2 ГБ, флэш-память 32 ГБ, камера 12 Мп, аккумулятор 1960 мАч, 1 SIM, цвет черный');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Samsung Galaxy S8 Dual SIM 64GB (черный бриллиант) [G950FD]', 1690, 10, CURRENT_TIMESTAMP, 'Android, экран 5.8" AMOLED (1440x2960), ОЗУ 4 ГБ, флэш-память 64 ГБ, карты памяти, камера 12 Мп, аккумулятор 3000 мАч, 2 SIM, цвет черный');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Apple iPhone 6 16GB Space Gray', 999, 10, CURRENT_TIMESTAMP, 'Apple iOS, экран 4.7" IPS (750x1334), ОЗУ 1 ГБ, флэш-память 16 ГБ, камера 8 Мп, аккумулятор 1810 мАч, 1 SIM, цвет темно-серый');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Apple iPhone 5s 16GB Space Gray', 620, 10, CURRENT_TIMESTAMP, 'Apple iOS, экран 4" IPS (640x1136), ОЗУ 1 ГБ, флэш-память 16 ГБ, камера 8 Мп, аккумулятор 1560 мАч, 1 SIM, цвет темно-серый');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Xiaomi Redmi 4A Gold', 221, 10, CURRENT_TIMESTAMP, 'Android, экран 5" IPS (720x1280), ОЗУ 2 ГБ, флэш-память 16 ГБ, карты памяти, камера 13 Мп, аккумулятор 3120 мАч, 2 SIM, цвет золотистый');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Huawei P8 lite 2017 Black', 465, 10, CURRENT_TIMESTAMP, 'Android, экран 5.2" IPS (1080x1920), ОЗУ 3 ГБ, флэш-память 16 ГБ, карты памяти, камера 12 Мп, аккумулятор 3000 мАч, 2 SIM, цвет черный');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Samsung Galaxy S7 Edge 32GB Black Onyx [G935F]', 1190, 10, CURRENT_TIMESTAMP, 'Android, экран 5.5" AMOLED (1440x2560), ОЗУ 4 ГБ, флэш-память 32 ГБ, карты памяти, камера 12 Мп, аккумулятор 3600 мАч, 1 SIM, цвет черный');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Xiaomi Redmi Note 4 32GB Grey', 360, 10, CURRENT_TIMESTAMP, 'Android, экран 5.5" IPS (1080x1920), ОЗУ 3 ГБ, флэш-память 32 ГБ, карты памяти, камера 13 Мп, аккумулятор 4100 мАч, 2 SIM, цвет серый');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Samsung Galaxy A5 (2017) Black [A520F]', 799, 10, CURRENT_TIMESTAMP, 'Android, экран 5.2" AMOLED (1080x1920), ОЗУ 3 ГБ, флэш-память 32 ГБ, карты памяти, камера 16 Мп, аккумулятор 3000 мАч, 2 SIM, цвет черный');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Samsung Galaxy J5 White [J500H/DS]', 299, 10, CURRENT_TIMESTAMP, 'Android, экран 5" AMOLED (720x1280), ОЗУ 1.5 ГБ, флэш-память 8 ГБ, карты памяти, камера 13 Мп, аккумулятор 2600 мАч, 2 SIM, цвет белый');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Apple iPhone SE 16GB Space Gray', 945, 10, CURRENT_TIMESTAMP, 'Apple iOS, экран 4" IPS (640x1136), ОЗУ 2 ГБ, флэш-память 16 ГБ, камера 12 Мп, аккумулятор 1642 мАч, 1 SIM, цвет темно-серый');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Xiaomi Mi 5 32GB White', 560, 10, CURRENT_TIMESTAMP, 'Android, экран 5.15" IPS (1080x1920), ОЗУ 3 ГБ, флэш-память 32 ГБ, камера 16 Мп, аккумулятор 3000 мАч, 2 SIM, цвет белый');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Nokia 3310 Dual SIM (серый)', 129, 10, CURRENT_TIMESTAMP, 'Nokia Series 30+, экран 2.4" TFT (240x320), флэш-память 16 Мб, карты памяти, камера 2 Мп, аккумулятор 1200 мАч, 2 SIM, цвет серый');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('MEIZU M3 Note 16GB Gray', 320, 10, CURRENT_TIMESTAMP, 'Android, экран 5.5" IPS (1080x1920), ОЗУ 2 ГБ, флэш-память 16 ГБ, карты памяти, камера 13 Мп, аккумулятор 4100 мАч, 2 SIM, цвет темно-серый');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Sony Xperia XZ Premium Deepsea Black [G8142]', 1800, 10, CURRENT_TIMESTAMP, 'Android, экран 5.5" IPS (2160x3840), ОЗУ 4 ГБ, флэш-память 64 ГБ, карты памяти, камера 19 Мп, аккумулятор 3230 мАч, 2 SIM, цвет черный');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Sony Xperia Z5 Compact Graphite Black', 819, 10, CURRENT_TIMESTAMP, 'Android, экран 4.6" IPS (720x1280), ОЗУ 2 ГБ, флэш-память 32 ГБ, карты памяти, камера 23 Мп, аккумулятор 2700 мАч, 1 SIM, цвет черный');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Sony Xperia Z2 Black', 646, 10, CURRENT_TIMESTAMP, 'Android, экран 5.2" IPS (1080x1920), ОЗУ 3 ГБ, флэш-память 16 ГБ, карты памяти, камера 20.7 Мп, аккумулятор 3200 мАч, 1 SIM, цвет черный');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Lenovo P70-A', 367, 10, CURRENT_TIMESTAMP, 'Android, экран 5" IPS (720x1280), ОЗУ 2 ГБ, флэш-память 16 ГБ, карты памяти, камера 13 Мп, аккумулятор 4000 мАч, 2 SIM, цвет синий');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('ZTE A610 Gold', 240, 10, CURRENT_TIMESTAMP, 'Android, экран 5" IPS (720x1280), ОЗУ 2 ГБ, флэш-память 16 ГБ, карты памяти, камера 8 Мп, аккумулятор 4000 мАч, 2 SIM, цвет золотистый');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Venso Reiv 500 Black', 193, 10, CURRENT_TIMESTAMP, 'Android, экран 5" IPS (720x1280), ОЗУ 1 ГБ, флэш-память 8 ГБ, карты памяти, камера 13 Мп, аккумулятор 4200 мАч, 2 SIM, цвет черный');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('ZTE Blade A510 Blue', 199, 10, CURRENT_TIMESTAMP, 'Android, экран 5" IPS (720x1280), ОЗУ 1 ГБ, флэш-память 8 ГБ, карты памяти, камера 13 Мп, аккумулятор 2200 мАч, 2 SIM, цвет синий');
+
+INSERT INTO PRODUCT (NAME, PRICE, QUANTITY, CRTD_TMS, DESCRIPTION) VALUES 
+('Lenovo Vibe K5 Plus Graphite Gray [A6020]', 280, 10, CURRENT_TIMESTAMP, 'Android, экран 5" IPS (1080x1920), ОЗУ 2 ГБ, флэш-память 16 ГБ, карты памяти, камера 13 Мп, аккумулятор 2750 мАч, 2 SIM, цвет темно-серый');
+
+INSERT INTO USER (USER_ID,FIRST_NAME,LAST_NAME,EMAIL,CRTD_TMS,UPDT_TMS,PHONE) VALUES 
+(18,'Alexander','Medvedev','alex-medvedev@tut.by','2017-06-17 14:17:29.000','2017-06-17 14:19:16.000','+375297505384')
+,(19,'SERGEY','TSAPOVSKI','TSAPOVSKI@YANDEX.RU','2017-06-17 14:20:39.000','2017-06-17 14:20:39.000','102')
+,(20,'Sergey','Morozov','smarozau@gmail.com','2017-06-17 14:21:47.000','2017-06-17 14:21:47.000','+375295566078')
+,(22,'Dmitry','Tsapovski','tsapovski@gmail.com','2017-06-17 14:22:52.000','2017-06-17 14:22:52.000','+375296592016')
+,(24,'Vladimir','Yashchenko','yashchenko-vladimir@tut.by','2017-06-17 14:23:57.000','2017-06-17 14:23:57.000','+375295773281')
+;
+
+INSERT INTO USER_ADDRESS(ADDR_ID, STREET_ADDR, CITY, USER_ID) VALUES (4, 'ul.Kalvariyskaya, 25', 'Minsk', 20);
+INSERT INTO USER_ADDRESS(ADDR_ID, STREET_ADDR, CITY, USER_ID) VALUES (5, 'ul. J. Kolasa, 1', 'Minsk', 18);
+INSERT INTO USER_ADDRESS(ADDR_ID, STREET_ADDR, CITY, USER_ID) VALUES (6, 'ul. Fr. Skorinu, 36', 'Minsk', 24);
+INSERT INTO USER_ADDRESS(ADDR_ID, STREET_ADDR, CITY, USER_ID) VALUES (10, 'BULVAR SHAHTIOROV', 'SOLIGORSK', 19);
+INSERT INTO USER_ADDRESS(ADDR_ID, STREET_ADDR, CITY, USER_ID) VALUES (11, 'ul. bogomolova', 'Solihorsk', 22);
+
+INSERT INTO PURCHASE_ORDER (ORDER_ID,USER_ID,STATUS_CD,DLRY_ADDR_ID, DLRY_DATE) VALUES 
+(30,18,1,5, CURRENT_DATE)
+,(31,20,1,4, CURRENT_DATE)
+,(32,24,1,6, CURRENT_DATE)
+,(33,22,1,5, CURRENT_DATE)
+,(34,22,1,5, CURRENT_DATE)
+;
+
+INSERT INTO PURCHASE_ORDER_PRODUCT (ORDER_ID,PRODUCT_CODE,PRODUCT_QUANTITY,PRODUCT_PRICE,DISCOUNT) VALUES 
+(30,43,2,1399,5)
+,(30,45,1,999,5)
+,(31,42,1,249,10)
+,(32,55,3,129,10)
+,(34,43,4,1399,20)
+,(34,44,4,1690,20)
+;
