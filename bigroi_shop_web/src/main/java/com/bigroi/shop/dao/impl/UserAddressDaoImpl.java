8850e02579308363d0508f0aff3a190e2ddc4039
hippi <hippi@DESKTOP-5SNOUQ3>
2017-07-15 12:21:48 +0300

    ertwgrg

--- bigroi_shop_web/src/main/java/com/bigroi/shop/dao/impl/UserAddressDaoImpl.java
+++ bigroi_shop_web/src/main/java/com/bigroi/shop/dao/impl/UserAddressDaoImpl.java
@@ -2,14 +2,22 @@ package com.bigroi.shop.dao.impl;
 
 import java.util.List;
 
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
 import com.bigroi.shop.dao.UserAddressDao;
 import com.bigroi.shop.model.UserAddress;
 
 public class UserAddressDaoImpl implements UserAddressDao {
+	
+	private NamedParameterJdbcTemplate npJdbcTemplate;
+
+	public void setNpJdbcTemplate(NamedParameterJdbcTemplate npJdbcTemplate) {
+		this.npJdbcTemplate = npJdbcTemplate;
+	}
 
 	@Override
 	public List<UserAddress> findByUserId(int userId) throws Exception {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
