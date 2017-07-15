dbb317ee90466fb698122ef189fc76dc42404705
нр615 <615@192.168.0.100>
2017-07-15 15:06:37 +0300

    controller updates

--- bigroi_shop_web/WebContent/WEB-INF/views/users.jsp
+++ bigroi_shop_web/WebContent/WEB-INF/views/users.jsp
@@ -1,13 +1,17 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>${title}</title>
 </head>
 <body>
-	<h1>Users</h1>
-	${message1}
+	<h1>${title}</h1>
+	<c:forEach var="u" items="${users}">
+		<br/>
+		${u.firstName} ${u.lastName}
+	</c:forEach>	
 </body>
 </html>
--- bigroi_shop_web/src/main/java/com/bigroi/shop/web/controller/UsersController.java
+++ bigroi_shop_web/src/main/java/com/bigroi/shop/web/controller/UsersController.java
@@ -1,5 +1,7 @@
 package com.bigroi.shop.web.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bigroi.shop.model.User;
 import com.bigroi.shop.service.UserService;
@@ -22,15 +25,16 @@ public class UsersController {
 
 	@RequestMapping(path="/users", method = RequestMethod.GET)
 	public String showUsers(ModelMap model) throws Exception {
-		logger.debug("show users: " + userService.findAll());
-		model.addAttribute("message1", "Hello Spring MVC Framework!");
+		model.addAttribute("title", "Users");
+		List<User> users = userService.findAll();
+		model.addAttribute("users", users);
 	    return "users";
 	}
 	
 	@RequestMapping(path="/user", method = RequestMethod.GET)
-	public String showUser(ModelMap model) throws Exception {
-		logger.debug("show users: " + userService.findAll());
-		User user = new User("John", "Doe", "john@doe.com", "+375");
+	public String showUser(@RequestParam int userId, ModelMap model) throws Exception {
+		logger.debug("show users: " + userService.findAll());		
+		User user = userService.findUserById(userId);
 		model.addAttribute("title", "User details");
 		model.addAttribute("user", user);
 	    return "user";
