0dc0586331fa25f47d2bba23d21acd2e33bce158
Alexander <Alexander@Alexander-PC>
2017-07-30 09:15:47 +0300

    disable csrf

--- bigroi_shop_web/src/main/java/com/bigroi/shop/service/impl/UserServiceImpl.java
+++ bigroi_shop_web/src/main/java/com/bigroi/shop/service/impl/UserServiceImpl.java
@@ -36,7 +36,7 @@ public class UserServiceImpl implements UserService {
 	@Transactional
 	public void save(User user) throws Exception {
 		userDao.save( user );
-		userAddressDao.save( user.getAdresses() );		
+//		userAddressDao.save( user.getAdresses() );		
 	}
 
 	@Override
